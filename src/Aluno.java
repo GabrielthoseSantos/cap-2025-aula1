efddd9461cdc95e941ec6970ddb1581a30d7d647
GabrielthoseSantos <160787128+GabrielthoseSantos@users.noreply.github.com>
2025-08-20 01:42:51 +0000

    Aula 19/08/2025

---
+++ src/Aluno.java
@@ -0,0 +1,22 @@
+package app;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+
+public class Aluno {
+
+   /* Atributos para a Classe Aluno */
+
+    private long id;
+    private String nome;
+    private int idade;
+
+   
+
+
+}
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,6 +3,14 @@ package app;
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
+
+
+        Aluno a1 = new Aluno();
+        a1.setId(1);
+        a1.setNome("Marcos");
+        a1.setIdade(45);
+
+        System.out.println(a1.getNome());
     }
 
 }
